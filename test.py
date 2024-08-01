640c853df1100ec6a9128b8022fdfa59b89fa165
Pavan K M <33823728+pavankm96@users.noreply.github.com>
2024-08-02 01:33:26 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,31 +1,20 @@
 from flask import Flask, request, render_template_string
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
-csrf = CSRFProtect(app)
 
-class MyForm(FlaskForm):
-    data = StringField('Data', validators=[DataRequired()])
-    submit = SubmitField('Submit')
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    form = MyForm()
-    if form.validate_on_submit():
-        data = form.data.data
-        return f"Data received: {data}"
     return render_template_string('''
-        <form method="post">
-            {{ form.hidden_tag() }}
-            {{ form.data.label }} {{ form.data(size=20) }}
-            {{ form.submit() }}
+        <form action="/submit" method="post">
+            <input type="text" name="data">
+            <input type="submit" value="Submit">
         </form>
-    ''', form=form)
+    ''')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    data = request.form['data']
+    return f"Data received: {data}"
 
 if __name__ == '__main__':
     app.run(debug=True)
-# fdf
